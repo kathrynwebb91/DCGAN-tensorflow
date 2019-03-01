34249468aa7d22f9f25cd3822671b56322ef0ab0
Kathryn Webb <kathryn.webb@akqa.com>
2019-03-01 11:23:50 +0000

    trying to fix zip file

--- download.py
+++ download.py
@@ -206,13 +206,14 @@ if __name__ == '__main__':
     download_sportslogos('./data')
 
 def fixBadZipfile(zipFile):  
- f = open(zipFile, 'r+b')  
- data = f.read()  
- pos = data.find('\x50\x4b\x05\x06') # End of central directory signature  
- if (pos > 0):  
+  f = open(zipFile, 'r+b')  
+  data = f.read()  
+  pos = data.find('\x50\x4b\x05\x06') # End of central directory signature  
+  if (pos > 0):  
      self._log("Trancating file at location " + str(pos + 22)+ ".")  
      f.seek(pos + 22)   # size of 'ZIP end of central directory record' 
      f.truncate()  
      f.close()  
- else:  
-     # raise error, file is truncated  
+  else:  
+    # raise error, file is truncated  
+    
